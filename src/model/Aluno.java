6dbd7fbfe205c8bb56b6b41d62bff0e26546a3b6
Leonardo Soares <leoferreirasoares@gmail.com>
2017-12-12 00:57:12 -0200

    subindo lista e alunos

--- src/model/Aluno.java
+++ src/model/Aluno.java
@@ -1,5 +1,68 @@
 package model;
 
 public class Aluno {
+	
+	private String id;
+	private String name;
+	private String email;
+	private String age;
+	private String gender;
+	private String comp;
+	private String city;
+	
+	public Aluno(String id, String name, String email, String age, String gender, String comp, String city) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.age = age;
+		this.gender = gender;
+		this.comp = comp;
+		this.city = city;
+	}
+
+	public Aluno(String name2, String email2, int idade, String cidade) {
+		this.name = name2;
+		this.email = email2;
+		this.age =""+idade;
+		this.city = cidade;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public String getComp() {
+		return comp;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	@Override
+	public String toString() {
+		return "Aluno [id=" + id + ", name=" + name + ", email=" + email + ", age=" + age + ", gender=" + gender
+				+ ", comp=" + comp + ", city=" + city + "]";
+	}
 
 }
